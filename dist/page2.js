51ad04050261ee7a838c4fcc81018214f6da1c61
kisunken <kkirmey@gmail.com>
2023-09-13 19:41:55 +0400

    update slider, complete sliderEnd button

--- dist/page2.js
+++ dist/page2.js
@@ -206,39 +206,37 @@ const sliderPrev = document.querySelector('.our-friends__slider-button_prev');
 const sliderNext = document.querySelector('.our-friends__slider-button_next');
 const sliderStart = document.querySelector('.our-friends__slider-button_start');
 const sliderEnd = document.querySelector('.our-friends__slider-button_end');
-const sliderIndicator = document.querySelector('.our-friends__slider-indicator');
+// const sliderIndicator = document.querySelector('.our-friends__slider-indicator');
 const sliderTrack = document.querySelector('.our-friends__slider-track');
 
 let flex = -1;
 let isAnimating = false;
-// let pageCounter = 1;
+let pageCounter = 1;
+let numberOfCards;
 
-// const changeNumberOfPage = (pageCounter) => {
-//   const sliderIndicator = document.querySelector('.our-friends__slider-indicator');
-//   const numberOfPages = 8;
+const numberOfPages = 8;
 
-//   if (sliderIndicator) {
-//     if (pageCounter <= numberOfPages) {
-//       sliderIndicator.innerText = pageCounter;
-//       sliderEnd.disabled = false;
-//       sliderNext.disabled = false;
-//     }
+const changeNumberOfPage = (pageCounter) => {
+  const sliderIndicator = document.querySelector('.our-friends__slider-indicator');
 
-//     if (pageCounter <= 1) {
-//       sliderPrev.disabled = true;
-//       sliderStart.disabled = true;
-//     } else {
-//       sliderStart.disabled = false;
-//       sliderPrev.disabled = false;
-//     }
+  if (sliderIndicator) {
+    sliderIndicator.innerHTML = pageCounter;
 
-//     if (pageCounter === numberOfPages) {
-//       sliderEnd.disabled = true;
-//       sliderNext.disabled = true;
-//     }
-//   }
+    if (pageCounter === 1) {
+      sliderStart.disabled = true;
+      sliderPrev.disabled = true;
+    } else if (pageCounter === numberOfPages) {
+      sliderEnd.disabled = true;
+      sliderNext.disabled = true;
+    } else {
+      sliderPrev.disabled = false;
+      sliderNext.disabled = false;
+      sliderStart.disabled = false;
+      sliderEnd.disabled = false;
+    }
+  }
 
-// };
+};
 
 const generateArrayOfRandomNumbers = (length, min, max) => {
   const arrayOfRandomNumbers = [];
@@ -258,6 +256,16 @@ const getNumberOfCards = () => {
   const page = document.querySelector('.our-friends__slider-page_our-pets');
   const screenWidth = window.innerWidth;
 
+  if (event.target === sliderEnd) {
+    console.log(pageCounter);
+    return numberOfPages - pageCounter;
+  }
+
+  if (event.target === sliderStart) {
+    console.log(pageCounter);
+    return pageCounter;
+  }
+
   if (page) return 1;
 
   if (screenWidth >= 1280) {
@@ -269,8 +277,6 @@ const getNumberOfCards = () => {
   }
 
 };
-
-// let numberOfCards = getNumberOfCards();
 
 const addSlideCards = async ({ parentSelector, numberOfPages }) => {
   const data = await (0,_utils__WEBPACK_IMPORTED_MODULE_0__.getResource)('../../../assets/json/pets.json');
@@ -311,54 +317,13 @@ const addSlideCards = async ({ parentSelector, numberOfPages }) => {
   (0,_modal__WEBPACK_IMPORTED_MODULE_2__.openPetsModal)(data, '.our-friends__wrapper');
 };
 
-// const moveSlider = ({ direction }) => {
-
-//   console.log(direction);
-//   if (isAnimating) return;
-
-//   isAnimating = true;
-
-//   const numberOfCards = getNumberOfCards();
-//   console.log(numberOfCards);
-
-//   const wrapper = document.querySelector('.our-friends__slider-wrapper');
-//   const wrapperWidth = wrapper.offsetWidth;
-
-//   const gapComputedStyle = window.getComputedStyle(sliderTrack);
-//   const gapValue = +gapComputedStyle.getPropertyValue('gap').slice(0, -2);
-
-//   const offset = wrapperWidth + gapValue;
-
-//   if (direction === 'next') {
-//     // pageCounter += 1;
-//     flex = -1;
-//     wrapper.style.justifyContent = 'flex-start';
-//     sliderTrack.style.transform = `translateX(${-offset}px)`;
-//   }
-
-//   if (direction === 'prev') {
-//     if (flex === -1) {
-//       for (let i = 0; i < numberOfCards; i++) {
-//         sliderTrack.appendChild(sliderTrack.firstElementChild);
-//       }
-//       flex = 1;
-//     }
-
-//     wrapper.style.justifyContent = 'flex-end';
-//     sliderTrack.style.transform = `translateX(${offset}px)`;
-
-//     // pageCounter -= 1;
-//   }
-
-//   // changeNumberOfPage(pageCounter);
-// };
-
 const moveSlider = ({ direction }) => {
+
   if (isAnimating) return;
 
   isAnimating = true;
 
-  const numberOfCards = getNumberOfCards();
+  numberOfCards = getNumberOfCards();
   console.log(numberOfCards);
 
   const wrapper = document.querySelector('.our-friends__slider-wrapper');
@@ -372,7 +337,6 @@ const moveSlider = ({ direction }) => {
   if (direction === 'next') {
     if (flex === 1) {
       for (let i = 0; i < numberOfCards; i++) {
-        console.log(i);
         sliderTrack.prepend(sliderTrack.lastElementChild);
       }
       flex = -1;
@@ -380,6 +344,7 @@ const moveSlider = ({ direction }) => {
 
     wrapper.style.justifyContent = 'flex-start';
     sliderTrack.style.transform = `translateX(${-offset}px)`;
+    pageCounter++;
 
   }
 
@@ -394,14 +359,14 @@ const moveSlider = ({ direction }) => {
 
     wrapper.style.justifyContent = 'flex-end';
     sliderTrack.style.transform = `translateX(${offset}px)`;
+    pageCounter--;
   }
 
+  changeNumberOfPage(pageCounter);
 };
 
 sliderTrack.addEventListener('transitionend', () => {
-  const numberOfCards = getNumberOfCards();
-
-  // console.log(numberOfCards);
+  console.log(numberOfCards);
 
   if (flex === -1) {
     for (let i = 0; i < numberOfCards; i++) {
@@ -409,6 +374,7 @@ sliderTrack.addEventListener('transitionend', () => {
     }
   } else {
     for (let i = 0; i < numberOfCards; i++) {
+      console.log(sliderTrack.lastElementChild);
       sliderTrack.prepend(sliderTrack.lastElementChild);
     }
   }
@@ -423,41 +389,23 @@ sliderTrack.addEventListener('transitionend', () => {
 
 });
 
-// window.addEventListener('resize', getNumberOfCards);
+window.addEventListener('resize', getNumberOfCards);
 
-window.addEventListener('resize', () => {
-  const numberOfCards = getNumberOfCards();
-  console.log(numberOfCards);
+sliderStart.addEventListener('click', () => {
+  moveSlider({ direction: 'prev' });
 });
 
 sliderPrev.addEventListener('click', () => {
   moveSlider({ direction: 'prev' });
 });
 
-sliderNext.addEventListener('click', () => moveSlider({ direction: 'next' }));
-// sliderEnd.addEventListener('click', () => moveSlider({ direction: 'next' }));
-// sliderStart.addEventListener('click', () => moveSlider({ direction: 'next' }));
+sliderNext.addEventListener('click', () => {
+  moveSlider({ direction: 'next' });
+});
 
-// sliderPrev.addEventListener('click', () => {
-//   numberOfCards = getNumberOfCards();
-//   moveSlider({ direction: 'prev' });
-//   changeNumberOfPage();
-//   console.log(numberOfCards);
-// });
-
-// sliderNext.addEventListener('click', () => {
-//   numberOfCards = getNumberOfCards();
-//   moveSlider({ direction: 'next' });
-//   changeNumberOfPage();
-//   console.log(numberOfCards);
-// });
-
-// sliderEnd.addEventListener('click', () => {
-//   numberOfCards = getNumberOfCards(7);
-//   moveSlider({ direction: 'next' });
-//   changeNumberOfPage(numberOfCards + 1);
-//   console.log(numberOfCards);
-// });
+sliderEnd.addEventListener('click', () => {
+  moveSlider({ direction: 'next' });
+});
 
 
 
